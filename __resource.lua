a95d66b4c7fd04074fc357ceff3e2fd56186d309
R3IC0P <80275730+R3IC0P@users.noreply.github.com>
2021-03-08 18:23:22 +0100

    music stopping
    Works but music in FiveM stoping after 3  sec. (sound-2 and sound-3)

---
+++ __resource.lua
@@ -0,0 +1,17 @@
+files {
+    'index.html',
+    'style.css',
+    'main.js',
+    'fontello.woff',
+    'cursor.png',
+    'loading-sound-1.ogg',
+    'loading.mp4',
+    'bgpart.mp4',
+    'loading-sound-2',
+    'loading-sound-3'
+}
+
+loadscreen 'index.html'
+client_script 'client.lua'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
---
+++ main.js
@@ -0,0 +1,73 @@
+window.onload = function () {
+   document.body.addEventListener("mousemove", function (event) {
+      let cursor = document.getElementById("cursor");
+
+      let x = event.pageX - cursor.width + 23;
+      let y = event.pageY - 1;
+
+      cursor.style.left = x;
+      cursor.style.top = y;
+   });
+}
+
+let audio = document.getElementById('music1');
+let prev_btn = document.querySelector('.prev-btn');
+let next_btn = document.querySelector('.next-btn');
+
+let currTrack = null;
+
+let songs = {
+   first: document.getElementById('music1'),
+   second: document.getElementById('music2'),
+   third: document.getElementById('music3')
+};
+
+let volume = document.querySelector('#volume-control');
+volume.addEventListener('change', function(e) {
+   songs.first.volume = e.currentTarget.value / 100;
+   songs.second.volume = e.currentTarget.value / 100;
+   songs.third.volume = e.currentTarget.value / 100;
+})
+
+function playMusic() {
+   if (audio.paused) {
+      audio.volume = 0.3;
+      audio.play();
+      currTrack++;
+      console.log(currTrack);
+   }
+}
+
+playMusic();
+
+function puaseSongs() {
+   songs.first.pause();
+   songs.second.pause();
+   songs.third.pause();
+}
+
+function next() {
+   puaseSongs();
+   if (currTrack == 1) {
+      songs.second.play();
+      currTrack++;
+      console.log(currTrack);
+   } else if (currTrack == 2) {
+      songs.third.play();
+      currTrack++;
+      console.log(currTrack);
+   }  else {}
+}
+
+function prev() {
+   puaseSongs();
+   if (currTrack == 3) {
+      songs.second.play();
+      currTrack--;
+      console.log(currTrack);
+   } else if (currTrack == 2) {
+      songs.first.play();
+      currTrack--;
+      console.log(currTrack);
+   } else {}
+}
